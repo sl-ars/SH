ccbd59ceb1ce71216d19066700d0e8308608e5e1
sl-ars <sl.ars@icloud.com>
2025-04-02 13:45:56 +0500

    Add Swagger/JWT/HealthCheck urls

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,22 +1,38 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.http import JsonResponse
+from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+from rest_framework.permissions import AllowAny
+from django.conf import settings
+from django.conf.urls.static import static
+
+from users.views import CustomRefreshView, CustomTokenObtainPairView, CustomVerifyView
+def health_check(_):
+    return JsonResponse({'status': 'ok'})
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+
+    path('djangoadmin/', admin.site.urls),
+                   
+    path("health/", health_check),
+    # ── JWT ────────
+    path("api/auth/token/refresh/", CustomRefreshView.as_view(), name="token-refresh"),
+    path("api/auth/token/verify/", CustomVerifyView.as_view(), name="token-verify"),
+    path("api/auth/login/", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
+
+    # Apps
+    path("api/user/", include("users.urls")),
+
+
+    # DRF Spectacular - Schema, Swagger UI, Redoc UI
+    path('api/schema/', SpectacularAPIView.as_view(permission_classes=[AllowAny]), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema', permission_classes=[AllowAny]),
+         name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema', permission_classes=[AllowAny]),
+         name='redoc'),
 ]
