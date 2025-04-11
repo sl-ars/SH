de3df161ab39d18faf0fa8063c81a9522860dbf5
Musilimov <g_mussilimov@kbtu.kz>
2025-04-11 15:49:56 +0500

    Finish analytics api

--- backend/users/storage.py
+++
@@ -1,9 +0,0 @@
-from core.storage import PublicAssetStorage, PrivateAssetStorage
-
-
-class AvatarStorage(PublicAssetStorage):
-    location = "media"
-
-
-class ResumeStorage(PrivateAssetStorage):
-    location = "resumes"
